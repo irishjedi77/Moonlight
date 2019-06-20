91bfcd812624c5f5a1c0ecb4f6fae399500a32f4
Erin Lyden <irishjedi77@mac.com>
2019-06-20 10:34:50 -0700

    pull

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 
 // pages for this product
-import Components from "views/Components/Components.jsx";
-import LandingPage from "views/LandingPage/LandingPage.jsx";
+import Components from "./views/Components/LandingPage/Components.jsx";
 import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
 import SignUpPage from "./views/Components/SignUpPage/SignUp.jsx";
@@ -17,7 +16,6 @@ var hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/landing-page" component={LandingPage} />
       <Route path="/profile-page" component={ProfilePage} />
       <Route path="/login-page" component={LoginPage} />
       <Route path="/signup-page" component={SignUpPage} />
